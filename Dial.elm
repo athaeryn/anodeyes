5da6612f671b870a725116f8ded0f0f0dc58760b
Mike Anderson <hello@mrmikea.com>
2016-05-10 21:22:53 -0500

    Extract Dial component

---
+++ Dial.elm
@@ -0,0 +1,52 @@
+module Dial exposing (Model, Msg, update, view)
+
+import Html exposing (..)
+import Html.Attributes as H exposing (..)
+import Html.Events exposing (onInput)
+
+import String
+import Result
+import Maybe
+
+type alias Note = Int
+
+type alias Model =
+  { value : Note
+  , label : String
+  }
+
+type Msg
+  = Reset
+  | SetValue Note
+
+
+eventValueToNote : String -> Note
+eventValueToNote value =
+  String.toInt value
+    |> Result.toMaybe
+    |> Maybe.withDefault 0
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Reset -> { model | value = 0 }
+    SetValue v -> { model | value = v }
+
+
+view : Model -> Html Msg
+view model =
+  let
+    handleInput =
+      \v -> SetValue (eventValueToNote v)
+  in
+    div []
+      [ input
+        [ type' "range",
+          H.min "0",
+          H.max "127",
+          value (toString model.value),
+          onInput handleInput
+        ] []
+      , h4 [] [ text model.label ]
+      ]
--- Main.elm
+++ Main.elm
@@ -2,64 +2,36 @@ module Anodeyes exposing (..)
 
 import Html exposing (..)
 import Html.App as Html
-import Html.Attributes as H exposing (..)
-import Html.Events exposing (onInput)
+import Html.Attributes exposing (..)
 
-import String
-import Result
-import Maybe
-
-type alias Note = Int
-
-type alias Model = Note
+import Dial
 
 type Msg
-  = Reset
-  | SetValue Note
+  = Volume Dial.Msg
+
+type alias Model =
+  { volume : Dial.Model
+  }
 
 
-eventValueToNote : String -> Note
-eventValueToNote value =
-  String.toInt value
-    |> Result.toMaybe
-    |> Maybe.withDefault 0
-
-
-noteView : Model -> Html Msg
-noteView model =
-  let
-    handleInput =
-      \v -> SetValue (eventValueToNote v)
-  in
-    div []
-      [ h2 [] [ text (toString model) ]
-      , input
-          [ type' "range",
-            H.min "0",
-            H.max "127",
-            value (toString model),
-            onInput handleInput
-          ] []
-      ]
-
-
-model : Model
-model = 0
-
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Reset -> 0
-    SetValue value -> value
+model =
+  { volume = Dial.Model 64 "volume"
+  }
 
 
 view : Model -> Html Msg
 view model =
   div []
-    [ img [ src "anodeyes.svg", width 400 ] []
-    , noteView model
+    [ img [ src "anodeyes.svg", width 300 ] []
+    , Html.map Volume (Dial.view model.volume)
     ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Volume volumeMsg ->
+      { model | volume = Dial.update volumeMsg model.volume }
 
 
 main =
