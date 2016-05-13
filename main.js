4d6e7a9e78a5a62796601984c52c744b2dc0dbb4
Mike Anderson <hello@mrmikea.com>
2016-05-12 23:42:39 -0500

    Set up node midi

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const electron = require('electron')
 const { app, BrowserWindow } = electron
 
+const midi = require('midi')
+const out = new midi.output()
+out.openPort(0)
+
 let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow({height: 800, width: 800})
   mainWindow.loadURL('file://' + __dirname + '/static/index.html')
   mainWindow.webContents.openDevTools()
+  setInterval(boop, 1000)
   mainWindow.on('closed', () => {
     mainWindow = null
   })
@@ -25,3 +30,18 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+let bop = false
+function boop () {
+  bop = !bop
+  // toggle LFO destination
+  sendCC(67, bop)
+}
+
+function sendCC (number, value) {
+  if (typeof value === 'boolean') {
+    value = value ? 127 : 0
+  }
+  out.sendMessage([176, number, value])
+}
+
